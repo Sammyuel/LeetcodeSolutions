7f865509d9ae5cabac53102d449abce965cd91bc
Sam Lee <sam.lee1@bell.ca>
2018-09-09 14:29:33 -0400

    reverseLinkedList2.py

--- removeNthNode.py
+++ removeNthNode.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+#test 
 class Solution(object):
     def removeNthFromEnd(self, head, n):
         if head == None:
