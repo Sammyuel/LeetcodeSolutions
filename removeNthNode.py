39823e9392f7fba93dc9c39d4c9a5263adff54b5
Sam Lee <sam.lee1@bell.ca>
2018-09-09 12:44:01 -0400

    removeNthNode

---
+++ removeNthNode.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        if head == None:
+            return 
+        nodes = []
+        previous = head
+        ptr = head
+        counter = 1
+        while ptr:
+            nodes.append(ptr)
+            ptr = ptr.next
+        deleteNode = len(nodes)-1 - (n-1)
+        if deleteNode == 0:
+            return head.next 
+        beforeNode = nodes[deleteNode-1]
+        if n > 1:
+            beforeNode.next = nodes[deleteNode + 1]
+        else:
+            beforeNode.next = None
+        return head
+        
